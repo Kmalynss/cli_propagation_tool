a7c853ae08485f048f69dac75f4f88e97da1d553
Kostya <kmalyn@softserveinc.com>
2016-05-25 11:04:29 +0300

    refactoring git_propagations.rb

--- jira_propagations/git_propagations.rb
+++ jira_propagations/git_propagations.rb
@@ -4,6 +4,7 @@ require 'pry'
 class GitPropagation
   DEFAULT_REPO = "coupa/coupa_development"
   DEFAULT_LABELS = ["needs review"]
+  DESCRIPTION_TEMPLATE = File.open('pr_description.md.erb') {|f| f.read }
 
   attr_accessor :access_token
   def initialize access_token
@@ -14,66 +15,89 @@ class GitPropagation
     @client ||= Octokit::Client.new(:access_token => access_token)
   end
 
-  def create_pr(test_hash)
+  class PullRequest
+    attr_reader :client, :title, :base_branch, :head_branch,:summary_of_issue, :summary_of_change, :testing_approach, :reviewers, :jira_main_link, :jira_propagation_link, :risk_level, :saved
+    def initialize client, data
+      @client = client
+      @base_branch = data[:base_branch]
+      @head_branch = data[:head_branch]
+      @title = data[:title]
+      @summary_of_issue = data[:summary_of_issue]
+      @summary_of_change = data[:summary_of_change]
+      @testing_approach = data[:testing_approach]
+      @reviewers = data[:reviewers]
+      @jira_main_link = data[:jira_main_link]
+      @jira_propagation_link = data[:jira_propagation_link]
+      @risk_level = data[:risk_level]
+    end
+
+    def body(template, data)
+      @body ||= ERB.new(DESCRIPTION_TEMPLATE).result(binding)
+    end
+
+    def save
+      begin
+       @saved = client.create_pull_request(DEFAULT_REPO, base_branch, head_branch, title, body)
+      rescue Octokit::UnprocessableEntity
+        puts "Can't create a PR to the #{base_branch} from #{head_branch}. Maybe you already created it?"
+        return false
+      end
+    end
+    
+
+    def saved?
+      saved.present?
+    end
+
+    def number
+      saved['number']
+    end
+
+    def url
+      saved['html_url']
+    end
+
+    def labels
+       DEFAULT_LABELS + ["risk level #{risk_level}" ]
+    end
+
+
+
+    def add_labels
+      client.add_labels_to_an_issue(DEFAULT_REPO, number, labels)
+    rescue Octokit::NotFound
+      puts "Can't add a label"
+    end
+  end
+
+  def create_prs(test_hash)
     created_pull_requests = []
     test_hash[:branches].keys.each do |head_branch|
       pr_hash = {}
-      created_pr = nil
 
       base_branch = test_hash[:branches][head_branch][:base_branch]
-      title = test_hash[:branches][head_branch][:title]
-      summary_of_issue = test_hash[:summary_of_issue]
-      summary_of_change = test_hash[:summary_of_change]
-      testing_approach = test_hash[:testing_approach]
-      reviewers = test_hash[:reviewers]
-      jira_main_link = test_hash[:branches][head_branch][:jira_main_link]
-      jira_propagation_link = test_hash[:branches][head_branch][:jira_propagation_link]
-      risk_level = test_hash[:risk_level]
+      pr = PullRequest.new(client,
+        :base_branch => test_hash[:branches][head_branch][:base_branch],
+        :title => test_hash[:branches][head_branch][:title],
+        :summary_of_issue => test_hash[:summary_of_issue],
+        :summary_of_change => test_hash[:summary_of_change],
+        :testing_approach => test_hash[:testing_approach],
+        :reviewers => test_hash[:reviewers],
+        :jira_main_link => test_hash[:branches][head_branch][:jira_main_link],
+        :jira_propagation_link => test_hash[:branches][head_branch][:jira_propagation_link],
+        :risk_level => test_hash[:risk_level]
+      )
 
-      body = formate_body(test_hash[:description_template], test_hash.merge(test_hash[:branches][head_branch]))
-      begin
-        created_pr = client.create_pull_request(DEFAULT_REPO, base_branch, head_branch, title, body)
-      rescue Octokit::UnprocessableEntity
-        puts "Can't create a PR to the #{base_branch} from #{head_branch}. Maybe you already created it?"
-      end
-
-      if created_pr
-        begin
-          add_labels_to_pr(created_pr, risk_level)
-        rescue Octokit::NotFound
-          puts "Can't add a label"
-        end
-
-        pr_hash[base_branch] = created_pr["html_url"]
-        created_pull_requests << pr_hash
+      if pr.save
+        pr.add_labels
+        created_pull_requests << { base_branch => pr.url }
       end
     end
 
     created_pull_requests
   end
 
-  private
-    def add_labels_to_pr(pr, risk_level)
-      labels_to_add = DEFAULT_LABELS
-      labels_to_add << "risk level #{risk_level}" unless risk_level.nil?
-      client.add_labels_to_an_issue(DEFAULT_REPO, pr["number"], labels_to_add)
-    end
 
-    def formate_body(template, data)
-      PrDescription.new(template, data).render
-    end
-
-  class PrDescription
-    attr_reader :template, :data
-    def initialize template, data
-      @template = template
-      @data = data
-    end
-
-    def render
-      ERB.new(template).result(binding)
-    end
-  end
 end
 
 # test_hash = {
